3ff0ffd50c4376c1b535a8f3b2d6cb2347511091
rui-vieira <ruimtvieira@gmail.com>
2022-11-26 23:24:58 +0000

    update styles with grid, no more issues with form input

--- src/container/popUpForm/FormComponent.tsx
+++ src/container/popUpForm/FormComponent.tsx
@@ -8,34 +8,35 @@ interface FormComponentProps {
     handleFetch: any;
 }
 
-const FormComponent = ({isLoading, handleFetch} : FormComponentProps) => {
+const FormComponent = ({ isLoading, handleFetch }: FormComponentProps) => {
 
     return (
         <>
-            <PopUpFormContainer>
-                <div>
-                    <FormTitle className="form-title">
-                        <h3>Send Invite</h3>
-                        <p>
-                            Send an invitation for an external user to use your internal shop
-                        </p>
-                    </FormTitle>
-                </div>
+            <PopUpFormContainer >
 
-                <FormContainer>
-                    <div className="form-group">
-                        <label htmlFor="profileLabel"> Profile </label>
-                        <div className="form-control">
-                            <select name="profile" id="profile">
-                                <option value="frontend">Developer front-end</option>
-                                <option value="backend">Developer back-end</option>
-                            </select>
-                        </div>
+                <FormTitle className="title-area">
+                    <h3 className="title-area-text">Send Invite</h3>
+                    <p className="title-area-phrase">
+                        Send an invitation for an external user to use your internal shop
+                    </p>
+                </FormTitle>
+
+
+                <FormContainer className="form-area" >
+
+                    <div className="form-profile-area" id="form-group">
+                        <label className="form-profile-title-area" htmlFor="profileLabel"> Profile </label>
+
+                        <select className="form-profile-selector" name="profile" id="profile">
+                            <option value="frontend">Developer front-end</option>
+                            <option value="backend">Developer back-end</option>
+                        </select>
                     </div>
 
-                    <div className="form-group ">
-                        <label htmlFor="email">Email</label>
-                        <div className="form-control">
+
+                    <div className="form-email-area"  id ="form-group">
+                        <label className="form-email-title-area" htmlFor="email">Email</label>
+                        <div className="form-email-input-area">
                             <input
                                 type="email"
                                 className="form-group-email"
@@ -46,14 +47,14 @@ const FormComponent = ({isLoading, handleFetch} : FormComponentProps) => {
                     </div>
                 </FormContainer>
 
-                <LoadingComp>
-                    {isLoading ? <LoadingSpinner /> : <LoadingReservedSpace />}
-   
-                </LoadingComp>
-                <SendInviteBtn onClick={handleFetch} disabled={isLoading} hidden={isLoading}>
-                    Invite
-                </SendInviteBtn>
+                <div className="button-invite-area">
+                    <LoadingComp>
+                        {isLoading ? <LoadingSpinner /> : <SendInviteBtn onClick={handleFetch} disabled={isLoading} hidden={isLoading}>
+                            Invite
+                        </SendInviteBtn>}
 
+                    </LoadingComp>
+                </div>
             </PopUpFormContainer>
         </>
     )
@@ -61,3 +62,108 @@ const FormComponent = ({isLoading, handleFetch} : FormComponentProps) => {
 
 export default FormComponent;
 
+
+
+/* display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 80%;
+  height: auto;
+
+  background: yellow;
+
+  h3{
+  font-family: 'Inter', sans-serif;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 24px;
+  letter-spacing: 0.01em;
+  }
+
+  p{
+    font-weight: 400;
+    font-size: 1rem;
+    line-height: 1.5rem;
+    color: #5C6670;
+    
+  div {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+  }
+
+  @media (max-width: 768px) {
+    p{
+      display: none;
+    }
+
+    */
+
+
+    /*
+  .form-area {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    margin-bottom: 14px;
+    font-size: 1.25rem;
+    line-height: 150%;
+    width: 80%;
+
+    label {
+      display: block;
+      font-size: 1 rem;
+      margin-bottom: 10px;
+    }
+    .form-control {
+      width: 99%;
+      height: 50px;
+      border: 2px solid #ddd;
+      border-radius: 5px;
+      color: #333;
+      &:focus {
+        outline: none;
+        border-color: #42bb98;
+      }
+    }
+    select {
+      width: 100%;
+      height: 50px;
+      border: none;
+      background-color: #fff;
+      padding: 0.4rem;
+      border-radius: 5px;
+      color: #AEB3B7;
+
+      &:focus {
+        outline: none;
+        border-color: #42bb98;
+        }
+    }
+    
+    .form-group-email{
+      width: 97%;
+      height: 48px;
+      border: none;
+      border-radius: 5px;
+      padding-left: 0.4rem;
+      
+    
+
+      &:focus {
+        outline: none;
+        border-color: #42bb98;
+      }
+
+      input {
+        width: 100%;
+        color: #AEB3B7;
+      }
+
+      @media (max-width: 768px) {
+       width: 10%;
+    }*/
